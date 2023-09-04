930b012507d61dad72f2e255617aa356906d5a4e
Lucas <lucascomamala@gmail.com>
2023-09-04 14:25:27 -0600

    button handler fix

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -6,10 +6,10 @@ import { CustomButtonProps } from "@/types"
 const CustomButton = ({ title, containerStyles, textStyles, btnType, rightIcon, handleClick, isDisabled }: CustomButtonProps) => {
   return (
     <button
-      disabled={false}
+      disabled={isDisabled || false}
       type={btnType || 'button'}
       className={`custom-btn ${containerStyles}`}
-      onClick={() => { }}
+      onClick={handleClick}
     >
       <span className={`flex-1 ${textStyles}`}>
         {title}
