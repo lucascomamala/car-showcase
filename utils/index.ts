0988dcbfb8922a99c5db516d98976f3576985de2
Lucas <lucascomamala@gmail.com>
2023-09-04 18:46:34 -0600

    generate car image call

--- utils/index.ts
+++ utils/index.ts
@@ -1,31 +1,51 @@
+import { CarProps } from "@/types"
+
 export async function fetchCars() {
   const headers = {
     "X-RapidAPI-Key": "5f05ba1d25mshf8a8d6ccc21f6e6p1e4ac0jsn263879edde86",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-  };
+  }
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera",
     {
       headers,
     }
-  );
+  )
 
-  const result = await response.json();
-  return result;
+  const result = await response.json()
+  return result
 }
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
-  const basePricePerDay = 50; // Base rental price per day in dollars
-  const mileageFactor = 0.1; // Additional rate per mile driven
-  const ageFactor = 0.05; // Additional rate per year of vehicle age
+  const basePricePerDay = 50 // Base rental price per day in dollars
+  const mileageFactor = 0.1 // Additional rate per mile driven
+  const ageFactor = 0.05 // Additional rate per year of vehicle age
 
   // Calculate additional rate based on mileage and age
-  const mileageRate = city_mpg * mileageFactor;
-  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  const mileageRate = city_mpg * mileageFactor
+  const ageRate = (new Date().getFullYear() - year) * ageFactor
 
   // Calculate total rental rate per day
-  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
 
-  return rentalRatePerDay.toFixed(0);
-};
+  return rentalRatePerDay.toFixed(0)
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage")
+  const { make, model, year } = car
+
+  url.searchParams.append(
+    "customer",
+    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ""
+  )
+  url.searchParams.append("make", make)
+  url.searchParams.append("modelFamily", model.split(" ")[0])
+  url.searchParams.append("zoomType", "fullscreen")
+  url.searchParams.append("modelYear", `${year}`)
+  // url.searchParams.append('zoomLevel', zoomLevel)
+  url.searchParams.append("angle", `${angle}`)
+
+  return `${url}`
+} 
